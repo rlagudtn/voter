c46e27754a59d6fc2d39be9507518bc14305beec
rlagudtn <rlagudtn4510@naver.com>
2021-09-28 23:32:36 +0900

    navbar css grid #21.09.28

--- Voter/src/main/frontend/src/js/NavMenu.js
+++ Voter/src/main/frontend/src/js/NavMenu.js
@@ -9,12 +9,44 @@ function NavMenu (props){
     let [searchToggle,changeSearchToggle]=useState(0);
     
     return (
-        <div>
-            <Navbar bg="light" variant="light" >
+        <div className="navbar-menu">
+            <div className="logo">
+            <img className="logo-img" src={logo} alt="" />
+                        <h1>VOTER</h1>
+            </div>
+            <div className="menu-list">
+                <li className="menu-item ">대선</li>
+                <li className="menu-item">국회의원</li>
+                <li className="menu-item">지방자치단체장 및 지방의회</li>
+            </div>
+            <InputGroup className="mb-0 search">
+                        <div >
+                            <FormControl
+                                placeholder="검색어를 입력하세요"
+                                aria-label="Recipient's username"
+                                onChange={(e)=>{
+                                    changeSearch(e.target.value);
+                                }}
+                            // aria-describedby="basic-addon2"
+                            />
+                            
+                        </div>
+                            <Link to={{
+                                pathname: '/NewsListPage',
+                                
+                            }}>
+                                <Button variant="outline-secondary" id="button-addon2" onClick={()=>{
+                                    changeKeyword(search);
+                                }}>
+                                    버튼
+                                </Button>
+                            </Link>
+                    </InputGroup>
+            {/* <Navbar bg="light" variant="light" >
                 <Container className="navbar-menu">
                     <div>
-                    <Navbar.Brand className="title" href="#home">
-                        <img className="logo" src={logo} alt="" />
+                    <Navbar.Brand className="logo" href="#home">
+                        <img className="logo-img" src={logo} alt="" />
                         <h1>VOTER</h1>
                     </Navbar.Brand>
 
@@ -53,7 +85,7 @@ function NavMenu (props){
                     </div>
                     
                 </Container>
-            </Navbar>
+            </Navbar> */}
         </div>
     );
 
