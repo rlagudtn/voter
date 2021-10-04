253f07a89871738bf6aed54791a68fb05dd848ae
rlagudtn <rlagudtn4510@naver.com>
2021-10-04 09:21:00 +0900

    modify news list page

--- Voter/src/main/frontend/src/js/NewsListPage.js
+++ Voter/src/main/frontend/src/js/NewsListPage.js
@@ -17,11 +17,18 @@ function NewsListPage(props){
  
     return (
         <div className="NewsListPage">
+            <div className="container">
+            <h3 className="search-title">
+                {props.keyword}<br/>뉴스 검색 결과
+            </h3>
             {
                 newsList.map((item,i)=>{
                     return <NewsPreview key={i} news={item}></NewsPreview>
                 })
             }
+            
+            </div>
+            
             
         </div>
     );
@@ -35,13 +42,18 @@ function NewsPreview(props){
     
     return (
         <div className="news-item">
-            <div className="subInfo">
-                <a className="provider"><b>{props.news.provider}</b></a>&nbsp;&nbsp;
-                <span>{parseTime(props.news.published_at)} </span>
-            </div> 
-            <h4 className="news-title">{props.news.title}</h4>
+            <img src="https://www.bigkinds.or.kr/resources/images/02100311/2021/10/02/02100311.20211002165632001.01.jpg" alt="" />
+            <h5 className="title">{props.news.title}</h5>
             <div className="content">{props.news.hilight}</div>
+            <div className="etc">
+                    <h5>{props.news.provider}</h5>
 
+                    <p>{props.news.byline}</p>
+                    {parseTime(props.news.published_at)}
+
+                
+            </div>
+            
         </div>
         
     );
